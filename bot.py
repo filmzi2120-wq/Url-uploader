92ab7d34cce1032dcc5c9ffbe3b217ae617ef17c
filmzi2120-wq <filmzi2120@gmail.com>
2025-11-17 12:16:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ REACTION_EMOJIS = [
 ]
 
 # Welcome image URL
-WELCOME_IMAGE = "https://gifs.alphacoders.com/gifs/view/220821"
+WELCOME_IMAGE = "https://ar-hosting.pages.dev/1763361985503.mp4"
 
 def format_time(seconds):
     """Format seconds to minutes and seconds"""
